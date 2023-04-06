5a1b8fde6ffc7bc80ebf33ab8773c09f95693788
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-04-06 18:45:05 +0530

    deploy on ec2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var port = process.env.PORT || 3000;
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello Github Actions - github");
+app.get("/", function (req, res) {
+  res.send("Hello Github Actions - github🥳");
 });
-app.listen(port, function(){
-    console.log(`Listening on port ${port}!`);
+
+app.get("/docker", function (req, res) {
+  res.send("Hello Docker running🔥");
+});
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}!`);
 });
