a1232d9e2cc439ee42cc37f5d7f93325a8fb75c2
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-04-06 21:43:51 +0530

    deploy on ec2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello Github Actions - github🥳123");
+  res.send("Hello Raghav Actions - github🥳123");
 });
 
 app.get("/docker", function (req, res) {
