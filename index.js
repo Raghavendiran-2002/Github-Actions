33ce959613ae4f409aa7f064f66c337a0dd216d5
Raghavendiran N <70228368+Raghavendiran-2002@users.noreply.github.com>
2023-04-06 13:35:06 +0530

    init actions

---
+++ index.js
@@ -0,0 +1,10 @@
+var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello Github Actions - github");
+});
+app.listen(port, function(){
+    console.log(`Listening on port ${port}!`);
+});
